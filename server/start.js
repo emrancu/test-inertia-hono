72a5eafde125d186841af33d30c1a49e3d07550f
AL EMRAN <emrancu1@gmail.com>
2025-08-14 20:10:00 +0600

    uop

--- server/start.js
+++
@@ -1,6 +0,0 @@
-import { serve } from '@hono/node-server'
-import app from './app.js'
-
-const port = process.env.PORT ? Number(process.env.PORT) : 3000
-serve({ fetch: app.fetch, port })
-console.log(`Hono listening on http://localhost:${port}`)
